e0d9e7e14b489b1d99ec64b45e5a6bd2471392d1
vishalkarad <vishalkarad777@gmail.com>
2020-03-14 17:15:53 +0530

    Added : given three number when second highest then return maximum

--- src/main/java/MaximumProblemUsingGenerics.java
+++ src/main/java/MaximumProblemUsingGenerics.java
@@ -13,4 +13,5 @@ public class MaximumProblemUsingGenerics {
         }
         return maxNumber;
     }
+
 }
--- src/test/java/MaximumProblemUsingGenericsTest.java
+++ src/test/java/MaximumProblemUsingGenericsTest.java
@@ -5,8 +5,13 @@ public class MaximumProblemUsingGenericsTest {
 
     MaximumProblemUsingGenerics maxnumber = new MaximumProblemUsingGenerics();
     @Test
-    public void given3Number_WhenFindMax_ThenTrue() {
+    public void givenThreeeNumber_WhenFirstMax_ThenTrue() {
         int max=maxnumber.maxnumber(10,20,30);
+        Assert.assertEquals(30,max);
+    }
+    @Test
+    public void givenThreeNumber_WhenSecondMaximum_ThenTrue() {
+        int max=maxnumber.maxnumber(10,30,20);
         Assert.assertEquals(30,max);
     }
 
