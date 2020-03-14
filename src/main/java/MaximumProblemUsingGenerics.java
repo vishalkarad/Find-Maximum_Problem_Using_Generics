39a343739b77aae68d830d5c32dfa9021dedb2f6
vishalkarad <vishalkarad777@gmail.com>
2020-03-14 15:56:05 +0530

    Added : Welcome to generics

---
+++ src/main/java/MaximumProblemUsingGenerics.java
@@ -0,0 +1,5 @@
+public class MaximumProblemUsingGenerics {
+    public static void main(String[] args) {
+        System.out.println("*************** Welcome to Generics ***************");
+    }
+}
