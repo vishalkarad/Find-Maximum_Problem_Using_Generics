86c05e1deb7859209541fa08931d440853a45894
vishalkarad <vishalkarad777@gmail.com>
2020-03-16 11:42:57 +0530

    Added : add max method and print maximum value

--- src/main/java/MaximumProblemUsingGenerics.java
+++ src/main/java/MaximumProblemUsingGenerics.java
@@ -5,17 +5,19 @@ public class MaximumProblemUsingGenerics <T extends Comparable <T>>{
     T secondVariable;
     T thardVariable;
 
+    // Set instance variable value using constructor
     public MaximumProblemUsingGenerics(T firstVariable, T secondVariable, T thardVariable) {
         this.firstVariable = firstVariable;
         this.secondVariable = secondVariable;
         this.thardVariable = thardVariable;
     }
-
+    // main method
     public static void main(String[] args) {
         System.out.println("*************** Welcome to Generics ***************");
     }
-
+    // Generate maximum value
     public <T extends Comparable <T>> T  getMaximumNumber(T firstValue,T secondValue, T thardValue){
+
         T maximumResult = firstValue;
         if(secondValue.compareTo(maximumResult)>0){
             maximumResult = secondValue;
@@ -25,8 +27,14 @@ public class MaximumProblemUsingGenerics <T extends Comparable <T>>{
         }
         return maximumResult;
     }
+    // pass instance value
     public T getMaximumNumber(){
-        return getMaximumNumber(firstVariable,secondVariable,thardVariable);
-
+        T maxvalue = getMaximumNumber(firstVariable,secondVariable,thardVariable);
+        printMaxValue(maxvalue);
+        return maxvalue;
+    }
+    // print max value
+    public void printMaxValue(T maxValue){
+        System.out.println(maxValue);
     }
 }
